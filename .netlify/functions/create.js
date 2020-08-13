0dde3dbe897490fc7b463ef411a179466aec3d7e
anandupender <anand.upender@gmail.com>
2020-08-13 16:32:21 -0400

    confetti

--- .netlify/functions/create.js
+++ .netlify/functions/create.js
@@ -1,24 +1,29 @@
 const Airtable = require('airtable')
 
 exports.handler = function(event, context, callback) {
-    var base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_KEY);
 
-    base('Main').create([JSON.parse(event.body)]
-        , function(err, records) {
-            if (err) { 
-                callback(err);
-            }
- 
-            const body = JSON.stringify({ record: records[0] })
-            const response = {
-              statusCode: 200,
-              body: body,
-              headers: {
-                'content-type': 'application/json',
-                'cache-control': 'Cache-Control: max-age=60, public'
-              }
-            }
-            callback(null, response)
-        });
+    if(JSON.parse(event.body).Name != "test"){
+        var base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_KEY);
+
+        base('Main').create([JSON.parse(event.body)]
+            , function(err, records) {
+                if (err) { 
+                    callback(err);
+                }
+    
+                const body = JSON.stringify({ record: records[0] })
+                const response = {
+                statusCode: 200,
+                body: body,
+                headers: {
+                    'content-type': 'application/json',
+                    'cache-control': 'Cache-Control: max-age=60, public'
+                }
+                }
+                callback(null, response)
+            });
+
+        }
+
 
 }
